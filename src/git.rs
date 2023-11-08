459de8b579c8a2af805d7cf7780c7899dbedf639
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-08 15:23:45 -0300

    1699467825/squeamish-art

---
+++ src/git.rs
@@ -0,0 +1,81 @@
+use names::Generator;
+use std::process::exit;
+use std::process::Command;
+
+pub fn gitpush() {
+    // TODO: use git commit -pm "message"
+
+    let has_commit = Command::new("git")
+        .arg("diff")
+        .arg("--quiet")
+        .arg("--exit-code")
+        .status()
+        .expect("Faild to execute git diff command");
+
+    if has_commit.success() {
+        println!("Nothing to commit");
+        exit(0)
+    }
+
+    let add_command = Command::new("git")
+        .arg("add")
+        .arg("-A")
+        .output()
+        .expect("Failed to execute git add command");
+
+    if !add_command.status.success() {
+        eprintln!("Error: Failed to add files to git repository.");
+        exit(1)
+    }
+
+    let commit_command = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(name_generator())
+        .output()
+        .expect("Failed to execute git commit command");
+
+    if !commit_command.status.success() {
+        eprintln!("Error: Failed to commit changes to git repository.");
+        exit(1)
+    }
+
+    let branch = Command::new("git")
+        .arg("rev-parse")
+        .arg("--abbrev-ref")
+        .arg("HEAD")
+        .output()
+        .expect("Failed to execute git rev-parse command");
+
+    if !branch.status.success() {
+        eprintln!("Error: Failed to get current branch name.");
+        exit(1)
+    }
+
+    let branch_name = String::from_utf8_lossy(&branch.stdout).trim().to_string();
+
+    let push_command = Command::new("git")
+        .arg("push")
+        .arg("origin")
+        .arg(branch_name)
+        .output()
+        .expect("Failed to execute git push command");
+
+    if !push_command.status.success() {
+        eprintln!("Error: Failed to push changes to git repository.");
+        exit(1)
+    }
+
+    println!("Successfully added, committed, and pushed changes!");
+}
+
+fn name_generator() -> String {
+    let now = std::time::SystemTime::now();
+    let now = now.duration_since(std::time::UNIX_EPOCH).unwrap();
+    let now = now.as_secs();
+
+    let mut generator = Generator::default();
+    let generator_output = generator.next().unwrap().to_string();
+
+    format!("{}/{}", now, generator_output)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,11 @@
-use names::Generator;
+mod version;
+use version::show_version;
 
-use std::process::exit;
-use std::process::Command;
-
-fn gitpush() {
-    // TODO: use git commit -pm "message"
-
-    let has_commit = Command::new("git")
-        .arg("diff")
-        .arg("--quiet")
-        .arg("--exit-code")
-        .status()
-        .expect("Faild to execute git diff command");
-
-    if has_commit.success() {
-        println!("Nothing to commit");
-        exit(0)
-    }
-
-    let add_command = Command::new("git")
-        .arg("add")
-        .arg("-A")
-        .output()
-        .expect("Failed to execute git add command");
-
-    if !add_command.status.success() {
-        eprintln!("Error: Failed to add files to git repository.");
-        exit(1)
-    }
-
-    let commit_command = Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg(name_generator())
-        .output()
-        .expect("Failed to execute git commit command");
-
-    if !commit_command.status.success() {
-        eprintln!("Error: Failed to commit changes to git repository.");
-        exit(1)
-    }
-
-    let branch = Command::new("git")
-        .arg("rev-parse")
-        .arg("--abbrev-ref")
-        .arg("HEAD")
-        .output()
-        .expect("Failed to execute git rev-parse command");
-
-    if !branch.status.success() {
-        eprintln!("Error: Failed to get current branch name.");
-        exit(1)
-    }
-
-    let branch_name = String::from_utf8_lossy(&branch.stdout).trim().to_string();
-
-    let push_command = Command::new("git")
-        .arg("push")
-        .arg("origin")
-        .arg(branch_name)
-        .output()
-        .expect("Failed to execute git push command");
-
-    if !push_command.status.success() {
-        eprintln!("Error: Failed to push changes to git repository.");
-        exit(1)
-    }
-
-    println!("Successfully added, committed, and pushed changes!");
-}
-
-fn name_generator() -> String {
-    let now = std::time::SystemTime::now();
-    let now = now.duration_since(std::time::UNIX_EPOCH).unwrap();
-    let now = now.as_secs();
-
-    let mut generator = Generator::default();
-    let generator_output = generator.next().unwrap().to_string();
-
-    format!("{}/{}", now, generator_output)
-}
-
-fn show_version() -> String {
-    const NAME: &str = env!("CARGO_PKG_NAME");
-    const VERSION: &str = env!("CARGO_PKG_VERSION");
-    format!("{} via 🦀 v{}/2023", NAME, VERSION)
-}
+mod git;
+use git::gitpush;
 
 fn main() {
     println!("{}", show_version());
+
     gitpush();
 }
