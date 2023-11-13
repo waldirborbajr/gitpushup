a40b165e1a5233b1100be4b43d2e1d691ae5549a
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-13 14:24:10 -0300

    chore(fic): CI warinig message

--- src/randmessage.rs
+++ src/randmessage.rs
@@ -4,5 +4,5 @@ pub fn rand_message() -> String {
     let mut generator = Generator::default();
     let generator_output = generator.next().unwrap().to_string();
 
-    format!("{}/{}", "Rand Message: ".to_string(), generator_output).to_string()
+    format!("{}/{}", r#"Rand Message: "#, generator_output)
 }
