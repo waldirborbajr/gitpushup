bd4fe56cf77155779dc60d443f2a0efcabccc4a4
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-13 09:22:55 -0300

    Rand Message: /illegal-society

--- src/randmessage.rs
+++ src/randmessage.rs
@@ -1,8 +1,8 @@
 use names::Generator;
 
 pub fn rand_message() -> String {
-    let mut generator = Generator::default();
-    let generator_output = generator.next().unwrap().to_string();
+  let mut generator = Generator::default();
+  let generator_output = generator.next().unwrap().to_string();
 
-    format!("{}/{}", r#"Rand Message: "#, generator_output)
+  format!("{}/{}", r#"Rand Message: "#, generator_output)
 }
