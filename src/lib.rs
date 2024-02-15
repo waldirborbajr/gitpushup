17ed99bc26d3b74f0ee02260f844121bf2b15416
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-15 08:01:10 -0300

    Rand Message: /malicious-woman

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,56 @@
+use anyhow::{anyhow, Result};
 use std::process::Command;
 
 // Validate if git is installed. If not, return an error message.
-pub fn find_git_command() -> Result<(), String> {
-	let gitcommand = Command::new("git").arg("--version").output().expect("Error");
-
-	match gitcommand.status.success() {
-		true => Ok(()),
-		false => Err("Error: 'git' command exited with non-zero status.".to_string()),
-	}
+pub fn validate_git_command() -> Result<()> {
+	Command::new("git")
+		.arg("--version")
+		.status()
+		.map_err(|err| anyhow!("Failed to check git availability: {}", err))
+		.and_then(
+			|status| {
+				if status.success() {
+					Ok(())
+				} else {
+					Err(anyhow!("git command not found"))
+				}
+			},
+		)
+	// let output = Command::new("git").arg("--version").output()?;
+	//
+	// if output.status.success() {
+	// 	Ok(())
+	// } else {
+	// 	Err(Error::msg("Git command not found"))
+	// }
 }
+
+// #[cfg(test)]
+// mod tests {
+// 	use super::*;
+//
+// 	#[test]
+// 	fn test_validate_git_command() {
+// 		assert!(validate_git_command().is_ok());
+// 	}
+// }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
-	fn test_git_exists() {
-		// Mock successful git execution
-		Command::new("gitA").arg("--version").output().expect("Failed to mock git command");
-
-		assert!(find_git_command().is_ok());
+	fn test_validate_git_exists_success() {
+		assert!(validate_git_command().is_ok()); // Assuming git is available
 	}
 
 	#[test]
-	fn test_git_not_found() {
-		// Mock unsuccessful git execution
-		Command::new("invalid_command")
-			.arg("--version")
-			.output()
-			.expect("Failed to mock invalid command");
+	#[should_panic]
+	fn test_validate_git_exists_fail() {
+		let mut cmd = Command::new("git");
+		cmd.env_remove("PATH"); // Simulate git not being available
+		assert!(cmd.arg("--version").status().is_err());
 
-		assert!(find_git_command().is_err());
+		validate_git_command().unwrap(); // Should panic with a custom error
 	}
 }
